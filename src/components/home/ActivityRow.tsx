fa9072456f705a5143280dda6d4fe67bad4e6d64
softgenai <admin@softgen.ai>
2025-07-11 04:55:55 +0000

    style(ui): Add custom scrollbar utilities

--- src/components/home/ActivityRow.tsx
+++ src/components/home/ActivityRow.tsx
@@ -1,4 +1,3 @@
-
 import { ActivityForHomepage } from "@/types/activity";
 import ActivityCard from "./ActivityCard";
 import Link from "next/link";
@@ -9,16 +8,18 @@ interface ActivityRowProps {
 
 export default function ActivityRow({ activities }: ActivityRowProps) {
   return (
-    <div className="flex overflow-x-auto space-x-4 pb-4 scrollbar-hide">
-      {activities.map((activity) => (
-        <Link 
-          key={activity.id} 
-          href={`/activities/${activity.slug || `activity-${activity.id}`}`}
-          className="flex-shrink-0 w-80"
-        >
-          <ActivityCard activity={activity} />
-        </Link>
-      ))}
+    <div className="relative">
+      <div className="flex overflow-x-auto space-x-4 pb-4 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300 hover:scrollbar-thumb-gray-400">
+        {activities.map((activity) => (
+          <Link 
+            key={activity.id} 
+            href={`/activities/${activity.slug || `activity-${activity.id}`}`}
+            className="flex-shrink-0 w-80"
+          >
+            <ActivityCard activity={activity} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
